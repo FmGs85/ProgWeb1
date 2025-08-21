1d9948fc502ff7516bef46b7249ebde9c96154e7
Fabio Melo <fabiomelogs85@gmail.com>
2025-08-21 18:57:37 -0300

    pequena correção

--- src/main/java/com/senac/projeto2/controller/UsuarioController.java
+++ src/main/java/com/senac/projeto2/controller/UsuarioController.java
@@ -1,36 +1,55 @@
 package com.senac.projeto2.controller;
 
+import com.senac.projeto2.entity.Usuario;
+import com.senac.projeto2.service.UsuarioService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/usuario")
-@Tag(name="Usuario", description="api para gerenciamento dos usuários do sistema")
+@Tag(name="Usuario", description="API para gerenciamento dos usuarios do sistema")
 public class UsuarioController {
+
+    private final UsuarioService usuarioService;
+
+    public UsuarioController(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
 
     @GetMapping("/listar")
     @Operation(summary = "Listar usuarios do sistema")
-    public String listar(){
-        return "Listando com sucesso";
+    public ResponseEntity<List<Usuario>> listar(){
+        return ResponseEntity.ok(usuarioService.listarUsuarios());
     }
 
-    @GetMapping("/listarPoridUsuario/{idUsuario}")
-    @Operation (summary = "Listar usuarios do sistema pelo id do usuário")
-    public String listarPorIdUsuario(@PathVariable("idUsuario") Integer idUsuario){
-        return "Listando um usuario por id com sucesso"+ idUsuario + " com sucesso";
+    @GetMapping("/listarPorIdUsuario/{idUsuario}")
+    @Operation(summary = "Listar usuarios do sistema pelo id do usuário")
+    public ResponseEntity<Usuario> listarPorIdUsuario(@PathVariable("idUsuario") Integer idUsuario){
+        Usuario usuario = usuarioService.listarUsuarioPorId(idUsuario);
+        if (usuario == null) {
+            return ResponseEntity.noContent().build();
+        }else{
+            return ResponseEntity.ok(usuario);
+        }
+
     }
 
     @PostMapping("/criar")
     public String criar(){
-        return "Usuário criado com sucesso!";
+        return "Usuario Criado com sucesso!";
     }
+
     @PutMapping("/atualizar")
     public String atualizar(){
-        return "Usuário atualizado com sucesso";
+        return  "Usuario atualizado com sucesso!";
     }
+
     @DeleteMapping("/apagar")
     public String apagar(){
-        return "Usuário apagado com sucesso";
+        return "Usuario apagado com sucesso!";
     }
 }
